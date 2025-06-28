863efd1e7b1f3c6eadf712af7dd7063230f341c6
odanaraujo <dansa.recteck@gmail.com>
2025-06-28 12:24:39 -0300

    feat: desafio novato xadrex

--- xadrez.c
+++ xadrez.c
@@ -5,28 +5,43 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    int numeroTorre;
+    int indexTorre = 0;
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("Informe quantas casas sua torre vai andar\n");
+    scanf("%d", &numeroTorre);
+    
+    // Move da torre para a direita
+    do {
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+        printf("Direita \n");
+        indexTorre++;
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    } while(indexTorre < numeroTorre);
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    
+    int numeroBispo;
+    int indexBispo = 0;
+    
+    printf("Informe quantas casas seu bispo vai andar\n");
+    scanf("%d", &numeroBispo);
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    // Move bisto na diagonal
+    for (indexBispo = 0; indexBispo < numeroBispo; indexBispo++) {
+        printf("Cima, Direita \n");
+    }
+
+
+    int numeroRainha;
+    int indexRainha = 0;
+    printf("Informe quantas casas sua rainha vai andar\n");
+    scanf("%d", &numeroRainha);
+
+    while(indexRainha < numeroRainha) {
+        printf("Esquerda\n");
+        indexRainha++;
+    }
 
     return 0;
 }
